cdd16b583d8ab974391b1192b7a5be884b64a830
changyanxin <2873005857@qq.com>
2019-11-12 21:29:48 +0800

    five

--- five/Blog/routes/index.js
+++ five/Blog/routes/index.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var path = require("path");
-var fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 const querystring = require("querystring");
 //get data
-var path = path.join(__dirname,'data.json');
-// 二进制数据
-var content = fs.readFileSync(path);
+var filePath = path.join(__dirname,'data.json');
+var content = fs.readFileSync(filePath);
 content = JSON.parse(content);
-// console.log(content);
+console.log(content);
+console.log(content.chapterList);
 // console.log(content.users[0].password);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/list',function(req,res,next){
-  console.log(req.body);
+router.post('/check',function(req,res,next){
+  // console.log(req.body);
   if(req.body.username == content.users[0].username 
     && req.body.password == content.users[0].password){
-      console.log("判断");
-      console.log("right");
+      // console.log("判断");
+      res.end("right");
   }else{
-    console.log("error");
+    res.end("error username and password");
   }
+});
+router.get('/list',function(req,res,next){
+  // res.write(content.chapterList);
+  res.render('list',{chapterList:content.chapterList});
 });
 
 module.exports = router;
